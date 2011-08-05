9a9349c41cb08aa4492e1130aba27061cc431ac2
Kenichi Yorozu <rozken@gmail.com>
2011-08-05 16:22:34 +0900

    Wanderの値がRandomになっていなかったのを修正． Random SeedをConstructor内で与えるよう訂正
    Signed-off-by: Kenichi Yorozu <rozken@gmail.com>

--- SASystem/Wander.hpp
+++ SASystem/Wander.hpp
@@ -21,9 +21,21 @@ public:
 		@return output モーターへの信号
 	*/
 	float Run(float signal);
+	/**
+		@brief 直進するか，ランダムに動くかを，一定の確率で選択する
+		@sa StochasitcSelector
+	*/
+	StochasticSelector* ss;
+	/**
+		@brief ランダムな動きを生成するのに用いる
+		@sa Random
+	*/
+	Random<boost::uniform_real<> >* random;
 };
 
 inline Wander::Wander(){
+	ss = new StochasticSelector(0.1);
+	random = new Random<boost::uniform_real<> >(0, 1);
 }
 
 inline float Wander::Run(float signal){
@@ -31,11 +43,9 @@ inline float Wander::Run(float signal){
 	///9割の確率で直進する
 	float rightSignal = 1.0f;
 	float leftSignal = 1.0f;
-	StochasticSelector ss(0.9);
-	if( !ss() ){	///残りの1割の確率で，ランダムに動く
-		Random<boost::uniform_real<> > random(0, 1);
-		rightSignal = random();
-		leftSignal = random();
+	if( ss->get() ){	///残りの1割の確率で，ランダムに動く
+		rightSignal = random->operator()();
+		leftSignal = random->operator()();
 	}
 
 	///両モーターの信号を一つにエンコード
