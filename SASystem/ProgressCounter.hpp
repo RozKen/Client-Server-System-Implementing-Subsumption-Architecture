b72ecc9ae7c289049d42ac44475451b18ba7cb87
RozKen <rozken@gmail.com>
2011-09-23 23:28:11 +0900

    ProgressCounterを実装
    Signed-off-by: RozKen <rozken@gmail.com>

---
+++ SASystem/ProgressCounter.hpp
@@ -0,0 +1,46 @@
+#ifndef ProgressCounter_HPP_
+#define ProgressCounter_HPP_
+
+#include "SAModule.h"
+#include "Constants.h"
+#include "EnvUpdater.h"
+
+/**
+	@class ProgressCounter
+	@brief 現在の位置を調べて返す
+	@sa SAModule
+ */
+class ProgressCounter : public SAModule{
+public:
+	/**
+		@brief Constructor
+		outputs[0] : 現在位置
+	 */
+	ProgressCounter(EnvUpdater* environment);
+	/**
+		@brief 現在の位置を調べてoutputsに出力
+	 */
+	void Run();
+	/**
+		@brief 現在の位置を調べて返す
+	 */
+	int getPosition();
+protected:
+	EnvUpdater* env;
+};
+
+inline ProgressCounter::ProgressCounter(EnvUpdater* environment)
+	: SAModule(0, 1), env(environment){
+}
+
+inline void ProgressCounter::Run(){
+	int pos = env->getPosition();
+	outputs[0] = pos;
+	return;
+}
+
+inline int ProgressCounter::getPosition(){
+	return env->getPosition();
+}
+
+#endif //ProgressCounter_HPP_
