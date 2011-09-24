e9631cdbaa8e6c788c337f62686c34426258a7dc
RozKen <rozken@gmail.com>
2011-09-24 16:17:48 +0900

    PositionUpdaterにおいて，バッテリー残量がないときの処理を追加
    Signed-off-by: RozKen <rozken@gmail.com>

--- SASystem/PositionUpdater.hpp
+++ SASystem/PositionUpdater.hpp
@@ -5,6 +5,8 @@
 #include "EnvUpdater.h"
 #include "Constants.h"
 
+#include <math.h>
+
 /**
 	@class PositionUpdater
 	@brief Controllerの信号をもとに現在位置を算出するモジュール
@@ -15,6 +17,7 @@ public:
 	/**
 		@brief Constructor
 		inputs[0] : progress
+		inputs[1] : battery : batteryがゼロになってしまえば，進めない
 	 */
 	PositionUpdater();
 	/**
@@ -26,18 +29,40 @@ public:
 		@sa position
 	 */
 	int getPosition();
+	/**
+		@brief getter of battery
+		@sa battery
+	 */
+	int getBattery();
 protected:
 	/**
 		@brief 現在位置
 	 */
 	int position;
+	/**
+		@brief 現在のバッテリー残量 [%]
+	 */
+	int battery;
 };
 
 inline PositionUpdater::PositionUpdater() : SAModule(1, 0), position(0){
 }
 
 inline void PositionUpdater::Run(){
-	position += inputs[0];
+	battery = inputs[1];
+	if(battery >= abs(inputs[0]) * 10){	//Battery残量が十分の場合
+		position += inputs[0];
+	}else{								//Battery残量が少ない場合
+		position += inputs[0] / abs(inputs[0]) * (battery / 10);
+	}
+}
+
+inline int PositionUpdater::getPosition(){
+	return position;
+}
+
+inline int PositionUpdater::getBattery(){
+	return battery;
 }
 
 #endif //PositionUpdater_HPP_
