f70077c47b167f8b45b4e6be20b33ac8aeb3d2c2
RozKen <rozken@gmail.com>
2011-11-29 19:28:21 +0900

    #17 Implemented.
    Signed-off-by: RozKen <rozken@gmail.com>

--- SASystem/ActPos.hpp
+++ SASystem/ActPos.hpp
@@ -5,35 +5,43 @@
 #include <iostream>
 /**
 	@class ActPos
-	@brief
-
+	@brief Robotの位置を移動させる．出力は現在からの相対距離
 	@author Kenichi Yorozu
 	@date 25th November 2011
  */
 class ActPos : public SAModule{
 public:
+	/**
+		@brief Default Constructor
+		Inputなどを追加していく.
+		Actuatorなので，Outputはない．
+	 */
 	ActPos();
-
+	/**
+		@brief 実行時の動作
+		@sa SAModule::Run()
+	 */
 	virtual void Run();
 };
 
 inline ActPos::ActPos(){
 	this->addInput("controlX");
 	this->addInput("controlY");
-	this->addIBoard("iDeltaX");
-	this->addIBoard("iDeltaY");
+	this->addFBoard("iDeltaX");
+	this->addFBoard("iDeltaY");
 }
 
 inline void ActPos::Run(){
 	//信号を入手
-	int dX = this->round(this->getInput(0));
+	float dX = this->getInput(0);
+	float dY = this->getInput(1);
+#ifdef _DEBUG
 	std::cout << "getInput0: " << this->getInput(0) << ": dX : " << dX << std::endl;
-	int dY = this->round(this->getInput(1));
-	std::cout << "getInput0: " << this->getInput(1) << ": dY : " << dY << std::endl;
+	std::cout << "getInput1: " << this->getInput(1) << ": dY : " << dY << std::endl;
+#endif //_DEBUG
 	//Robotの内部に出力信号を格納
-	this->setIBoard(0, dX);
-	this->setIBoard(1, dY);
-	//Log();
+	this->setFBoard(0, dX);
+	this->setFBoard(1, dY);
 }
 
 #endif	//_Act_Pos_HPP_
